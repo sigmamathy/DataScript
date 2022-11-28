3ca60f4b5261ad6e8bcfd93387784a70b60336a1
sigmamathy <makgsum@gmail.com>
2022-11-29 01:43:44 +0800

    Changes syntax and add debug message support

--- DataScript/DataScriptReader.cpp
+++ DataScript/DataScriptReader.cpp
@@ -1,192 +1,48 @@
-#include "./DataScriptReader.hpp"
+#include "DataScriptReader.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
-enum class DsReader::PrimitiveType : char
+enum : char
 {
-	INT,
-	UINT,
-	LONG,
-	ULONG,
-	FLOAT,
-	DOUBLE,
-	STRING
+	DsInt,
+	DsUint,
+	DsLong,
+	DsUlong,
+	DsFloat,
+	DsDouble,
+	DsString
 };
 
-DsReader::DsReader(std::string const& file)
-	: read_ptr(0)
+enum : char
 {
-#define VALIDATE(x) if (!(x)) { status = DsStatus::Bad; return; }
-	std::ifstream ifs(file);
-	VALIDATE(ifs.is_open());
-	status = DsStatus::Good;
+	DsReadNone,
+	DsReadType,
+	DsReadTypelength,
+	DsReadLabel,
+	DsReadString
+};
 
-	typelist.reserve(10);
-	typelist["int"].primitives = { PrimitiveType::INT };
-	typelist["uint"].primitives = { PrimitiveType::UINT };
-	typelist["long"].primitives = { PrimitiveType::LONG };
-	typelist["ulong"].primitives = { PrimitiveType::ULONG };
-	typelist["float"].primitives = { PrimitiveType::FLOAT };
-	typelist["double"].primitives = { PrimitiveType::DOUBLE };
-	typelist["string"].primitives = { PrimitiveType::STRING };
-	unsigned type_prim_offset = 0u;
+bool andneq(auto&& x, auto&&... args) { return ((x != args) && ...); }
+bool oreq(auto&& x, auto&&... args) { return ((x == args) || ...); }
 
-	char temp;
-	std::string type = "";
-	type.reserve(10);
-	size_t typelength = 1, begin_ptr = 0;
-	enum class ReadMode { None, Type, TypeLength, Value, StringValue, ControlStatement }
-	readmode = ReadMode::None;
-	constexpr auto is_alphabet_char = [](char c) -> bool { return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'); };
-	bool on_line_begin = true;
-	std::vector<std::string> commands;
-	commands.reserve(10);
-
-	for (char prev = '\0'; ifs.get(temp); prev = temp)
+static char DsStringToType(std::string const& str)
+{
+	static std::unordered_map<std::string, char> lookup =
 	{
-#define STRING_MODE_ACTION if (readmode == ReadMode::StringValue) { rdatas.back().value += temp; break; }
-		switch (temp)
-		{
-		case '[': // begin type read
-			STRING_MODE_ACTION;
-			VALIDATE(readmode == ReadMode::None);
-			readmode = ReadMode::Type;
-			type = "";
-			typelength = 1;
-			break;
-
-		case ']': // end type read
-			STRING_MODE_ACTION;
-			VALIDATE(readmode == ReadMode::Type || readmode == ReadMode::TypeLength);
-			readmode = ReadMode::None;
-			VALIDATE(typelist.count(type));
-			type_prim_offset = 0u;
-			break;
-
-		case '(': // begin value read
-			STRING_MODE_ACTION;
-			VALIDATE(readmode == ReadMode::None);
-			readmode = ReadMode::Value;
-			break;
-
-		case ')': // end value read
-			STRING_MODE_ACTION;
-			VALIDATE(readmode == ReadMode::Value);
-			VALIDATE((!typelength && (rdatas.size() - begin_ptr) % typelist[type].primitives.size() == 0)
-				|| rdatas.size() - begin_ptr == typelength * typelist[type].primitives.size());
-			VALIDATE(ValueValidate(begin_ptr));
-			begin_ptr = rdatas.size();
-			readmode = ReadMode::None;
-			break;
-
-		case '\n':
-			on_line_begin = true;
-			if (readmode == ReadMode::ControlStatement)
-			{
-				HandleCommand(commands);
-				readmode = ReadMode::None;
-			}
-			break;
-
-		case '#':
-			STRING_MODE_ACTION;
-			VALIDATE(on_line_begin && readmode == ReadMode::None);
-			readmode = ReadMode::ControlStatement;
-			commands.clear();
-			break;
-
-		default:
-			// read characters
-			switch (readmode)
-			{
-				using enum ReadMode;
-
-			case Type:
-				if (temp == ' ') break;
-				if (temp == ':') { readmode = TypeLength; typelength = 0; break; }
-				VALIDATE(prev != ' ' || type.empty());
-				type += temp;
-				break;
-
-			case TypeLength:
-				if (temp == ' ') break;
-				VALIDATE(temp >= '0' && temp <= '9');
-				typelength = typelength * 10 + temp - '0';
-				break;
-
-			case Value:
-				if (temp == ' ' || temp == ',' || temp == '\t') break;
-				if (prev == '\n' || prev == '\t' || prev == '(' || prev == ' ' || prev == ',')
-				{
-					rdatas.emplace_back(typelist.at(type).primitives[type_prim_offset++], "")
-						.value.reserve(30);
-					if (type_prim_offset == typelist.at(type).primitives.size()) type_prim_offset = 0u;
-				}
-				rdatas.back().value += temp;
-				if (temp == '"')
-					readmode = StringValue;
-				break;
-
-			case StringValue:
-				if (temp != '\\' || prev == '\\')
-					rdatas.back().value += temp;
-				if (temp == '"' && prev != '\\')
-					readmode = Value;
-				break;
-
-			case ControlStatement:
-				if (temp == ' ' || temp == '\t' || temp == ',') break;
-				if (prev == ' ' || prev == '\t' || prev == ',' || prev == '#')
-					commands.emplace_back("").reserve(10);
-				commands.back() += temp;
-				break;
-			}
-		}
-		if (temp != ' ' && temp != '\t' && temp != '\n')
-			on_line_begin = false;
-	}
-	VALIDATE(readmode == ReadMode::None);
-	if (rdatas.empty())
-		status = DsStatus::Eof;
-#undef VALIDATE
-#undef STRING_MODE_ACTION
-	ifs.close();
-}
-
-DsStatus DsReader::Status() const
-{
-	return status;
-}
-
-size_t DsReader::Position() const
-{
-	return read_ptr;
-}
-
-bool DsReader::ValueValidate(size_t begin_ptr) const
-{
-	for (size_t i = begin_ptr; i < rdatas.size(); i++)
-	{
-		bool good = false;
-		switch (rdatas[i].type)
-		{
-			using enum PrimitiveType;
-		case INT:		good = ReadNumber<int>(rdatas[i].value, nullptr); break;
-		case UINT:		good = ReadNumber<unsigned>(rdatas[i].value, nullptr); break;
-		case LONG:		good = ReadNumber<long long>(rdatas[i].value, nullptr); break;
-		case ULONG:		good = ReadNumber<unsigned long long>(rdatas[i].value, nullptr); break;
-		case FLOAT:		good = ReadNumber<float>(rdatas[i].value, nullptr); break;
-		case DOUBLE:	good = ReadNumber<double>(rdatas[i].value, nullptr); break;
-		case STRING:	good = ReadString(rdatas[i].value, nullptr); break;
-		}
-
-		if (!good) return false;
-	}
-	return true;
+		{ "int", DsInt },
+		{ "uint", DsUint },
+		{ "long", DsLong },
+		{ "ulong", DsUlong },
+		{ "float", DsFloat },
+		{ "double", DsDouble },
+		{ "string", DsString }
+	};
+	return lookup.count(str) ? lookup.at(str) : -1;
 }
 
 template<class Type>
-bool DsReader::ReadNumber(std::string const& str, Type* output) const
+static bool ReadNumber(std::string const& str, Type* var)
 {
 	if (std::is_unsigned_v<Type> && str[0] == '-')
 		return false;
@@ -195,78 +51,257 @@ bool DsReader::ReadNumber(std::string const& str, Type* output) const
 	Type x;
 	ss >> x;
 	if (!ss.eof() || ss.fail()) return false;
-	if (output) *output = x;
+	if (var) *var = x;
 	return true;
 }
 
-bool DsReader::ReadString(std::string const& str, std::string* output) const
+static bool ReadString(std::string const& str, std::string* output)
 {
 	if (str.length() < 2 || str[0] != '"' || str.back() != '"') return false;
 	if (output) *output = str.substr(1, str.length() - 2);
 	return true;
 }
 
-bool DsReader::HandleCommand(std::vector<std::string> const& args)
+static bool ValueValidate(char type, std::string const& val)
 {
-	if (args.empty())
-		return false;
-	if (args[0] == "section")
+	switch (type)
 	{
-		if (args.size() != 2) return false;
-		sections[args[1]] = rdatas.size();
-		return true;
+		case DsInt:		return ReadNumber<int>(val, nullptr);
+		case DsUint:	return ReadNumber<unsigned>(val, nullptr);
+		case DsLong:	return ReadNumber<long long>(val, nullptr);
+		case DsUlong:	return ReadNumber<unsigned long long>(val, nullptr);
+		case DsFloat:	return ReadNumber<float>(val, nullptr);
+		case DsDouble:	return ReadNumber<double>(val, nullptr);
+		case DsString:	return ReadString(val, nullptr);
+		default:		return false;
 	}
-	if (args[0] == "typedef")
+}
+
+DsReader::DsReader(std::string const& filepath)
+	: state(0), read_pointer(0)
+{
+#define PreValidate(x, msg) if (!(x)) { state |= DsBad; message = msg; return; }
+	std::ifstream ifs(filepath);
+	PreValidate(ifs.is_open(), "Cannot open file \"" + filepath + "\"");
+
+	char readmode = DsReadNone;
+	unsigned line = 1u;
+#define Validate(x, msg) PreValidate(x, msg " [line:" + std::to_string(line) + "]")
+	raw_datas.reserve(100);
+	std::string type_s, typelength_s, label_s;
+	std::vector<char> types;
+	types.reserve(10);
+	unsigned type_offset = 0u;
+
+	for (char prev = '\0', temp; ifs.get(temp); prev = temp)
 	{
-		if (args.size() < 4 && args[2] != "=") return false;
-		auto& x = typelist[args[1]];
-		x.primitives.reserve(20);
-		for (int i = 3; i < args.size(); i++)
+		// Reading string
+		if (readmode == DsReadString)
 		{
-			if (!typelist.count(args[i]))
-				return false;
-			for (auto& y : typelist.at(args[i]).primitives)
-				x.primitives.emplace_back(y);
+			if (temp == '\n') ++line;
+			if (temp != '\\' || prev == '\\')
+				raw_datas.back().value += temp;
+			if (temp == '"' && prev != '\\')
+				readmode = DsReadNone;
+			continue;
 		}
-		return true;
+		// Check characters
+		switch (temp)
+		{
+		case '(':
+			// Reading type begin
+			Validate(readmode == DsReadNone, "Unexpected '(' found");
+			Validate(type_offset == 0u, "Numbers of value does not match the type defined");
+			if (andneq(prev, ' ', '\t', ',', '\n', ')') && !raw_datas.empty())
+				Validate(ValueValidate(raw_datas.back().type, raw_datas.back().value), "Invalid value found");
+			readmode = DsReadType;
+			type_s = "";
+			typelength_s = "";
+			types.clear();
+			type_offset = 0u;
+			break;
+
+		case ')':
+			// Reading type end
+			if (readmode == DsReadType)
+			{
+				if (andneq(prev, ' ', '\t', ',', '('))
+					Validate(types.emplace_back(DsStringToType(type_s)) != -1, "Invalid typename found");
+			}
+			else if (readmode == DsReadTypelength)
+			{
+				std::stringstream ss;
+				ss << typelength_s;
+				unsigned x;
+				ss >> x;
+				Validate(ss.eof() && !ss.fail() && x > 0u, "Invalid typelength found");
+				for (unsigned i = 0; i < (x - 1); i++)
+					types.emplace_back(types.back());
+			}
+			else
+				Validate(false, "Unexpected ')' found");
+			readmode = DsReadNone;
+			break;
+
+		case '[':
+			Validate(readmode == DsReadNone, "Unexpected '[' found");
+			readmode = DsReadLabel;
+			break;
+
+		case ']':
+			Validate(readmode == DsReadLabel, "Unexpected ']' found");
+			Validate(!labels.count(label_s), "Label redefinition");
+			labels[label_s] = (unsigned) raw_datas.size();
+			readmode = DsReadNone;
+			break;
+
+		case '\n':
+			// new line character
+			++line;
+			if (readmode == DsReadType && andneq(prev, ' ', '\t', ',', '(', '\n'))
+			{
+				types.emplace_back(DsStringToType(type_s));
+				Validate(types.back() != -1, "Invalid typename found");
+				type_s = "";
+			}
+			else if (readmode == DsReadTypelength && andneq(prev, ' ', '\t', ',', ':', '\n'))
+			{
+				std::stringstream ss;
+				ss << typelength_s;
+				unsigned x;
+				ss >> x;
+				Validate(ss.eof() && !ss.fail() && x > 0u, "Invalid typelength found");
+				for (unsigned i = 0; i < (x - 1); i++)
+					types.emplace_back(types.back());
+				typelength_s = "";
+				readmode = DsReadType;
+			}
+			break;
+
+		default:
+			// Reading normal characters
+			switch (readmode)
+			{
+			case DsReadType:
+				// Reading type
+				if (temp == ':' || (oreq(temp, ' ', '\t', ',') && andneq(prev, ' ', '\t', ',', '(', '\n')))
+				{
+					if (temp == ':')
+					{
+						readmode = DsReadTypelength;
+						if (oreq(prev, ' ', '\t', ',', '\n'))
+							break;
+					}
+					types.emplace_back(DsStringToType(type_s));
+					Validate(types.back() != -1, "Invalid typename found");
+					type_s = "";
+					break;
+				}
+				if (andneq(temp, ' ', '\t', ','))
+					type_s += temp;
+				break;
+
+			case DsReadTypelength:
+				// Reading type length
+				if (oreq(temp, ' ', '\t', ',', '\n') && andneq(prev, ' ', '\t', ',', ':', '\n'))
+				{
+					std::stringstream ss;
+					ss << typelength_s;
+					unsigned x;
+					ss >> x;
+					Validate(ss.eof() && !ss.fail() && x > 0u, "Invalid typelength found");
+					for (unsigned i = 0; i < (x - 1); i++)
+						types.emplace_back(types.back());
+					typelength_s = "";
+					readmode = DsReadType;
+				}
+				if (andneq(temp, ' ', '\t', ',', '\n'))
+					typelength_s += temp;
+				break;
+
+			case DsReadLabel:
+				Validate(temp != '\n', "newline character is not allowed in label declaration")
+				label_s += temp;
+				break;
+
+			case DsReadNone:
+				// Reading values
+				if (oreq(temp, ' ', '\t', ','))
+					break;
+				if (oreq(prev, ' ', '\t', ',', '\n', ')', ']'))
+				{
+					if (!raw_datas.empty())
+						Validate(ValueValidate(raw_datas.back().type, raw_datas.back().value), "Invalid value found");
+					raw_datas.emplace_back(types[type_offset++], "").value.reserve(10);
+					if (type_offset == types.size()) type_offset = 0u;
+				}
+				raw_datas.back().value += temp;
+				if (temp == '"')
+					readmode = DsReadString;
+				break;
+			}
+		}
 	}
 
-	return false;
+	Validate(type_offset == 0u, "Numbers of value does not match the type defined");
+	Validate(readmode == DsReadNone, "Statements not enclosed");
+	if (raw_datas.empty()) state |= DsEof;
+	else Validate(ValueValidate(raw_datas.back().type, raw_datas.back().value), "Invalid value found");
+#undef PreValidate
+#undef Validate
+	ifs.close();
 }
 
-size_t DsReader::SectionPosition(std::string const& name) const
+DsReader::operator unsigned char() const
 {
-	return sections.count(name) ? sections.at(name) : static_cast<size_t>(-1);
+	return state;
 }
 
-void DsReader::Goto(size_t position)
+std::string DsReader::Message() const
 {
-	read_ptr = position;
-	if (position >= rdatas.size() && status != DsStatus::Bad)
-		status = DsStatus::Eof;
+	return message;
 }
 
-void DsReader::Goto(std::string const& name)
+void DsReader::Goto(unsigned location)
 {
-	if (!sections.count(name)) return;
-	read_ptr = sections.at(name);
-	if (read_ptr >= rdatas.size() && status != DsStatus::Bad)
-		status = DsStatus::Eof;
+	read_pointer = location;
+	if (location >= raw_datas.size())
+		state |= DsEof;
 }
 
-#define READ_IMPL(ty, pty, method) template<> void DsReader::Read<ty>(ty& val)\
-	{ if (status == DsStatus::Bad || status == DsStatus::Eof) return;\
-	if (rdatas[read_ptr].type != PrimitiveType::pty) { status = DsStatus::BadType; return; }\
-	method(rdatas[read_ptr++].value, &val); if (read_ptr == rdatas.size()) status = DsStatus::Eof; }
+unsigned DsReader::Pointer() const
+{
+	return read_pointer;
+}
 
-READ_IMPL(int, INT, ReadNumber);
-READ_IMPL(unsigned, UINT, ReadNumber);
-READ_IMPL(long, INT, ReadNumber);
-READ_IMPL(unsigned long, UINT, ReadNumber);
-READ_IMPL(long long, LONG, ReadNumber);
-READ_IMPL(unsigned long long, ULONG, ReadNumber);
-READ_IMPL(float, FLOAT, ReadNumber);
-READ_IMPL(double, DOUBLE, ReadNumber);
-READ_IMPL(std::string, STRING, ReadString);
+void DsReader::Goto(std::string const& label)
+{
+	if (!labels.count(label)) return;
+	read_pointer = labels.at(label);
+	if (read_pointer >= raw_datas.size())
+		state |= DsEof;
+}
 
-#undef READ_IMPL
+unsigned DsReader::Label(std::string const& name) const
+{
+	return labels.count(name) ? labels.at(name) : static_cast<unsigned>(-1);
+}
+
+#define DefineReadOperator(ty, ety, method) template<>\
+DsReader& DsReader::operator>>(ty& var) {\
+	if (state & DsBad || state & DsEof) return *this;\
+	if (raw_datas[read_pointer].type != ety) { state |= DsBadtype; return *this; }\
+	method(raw_datas[read_pointer++].value, &var);\
+	if (read_pointer >= raw_datas.size()) state |= DsEof;\
+	return *this;\
+}
+
+DefineReadOperator(int, DsInt, ReadNumber);
+DefineReadOperator(unsigned, DsUint, ReadNumber);
+DefineReadOperator(long long, DsLong, ReadNumber);
+DefineReadOperator(unsigned long long, DsUlong, ReadNumber);
+DefineReadOperator(float, DsFloat, ReadNumber);
+DefineReadOperator(double, DsDouble, ReadNumber);
+DefineReadOperator(std::string, DsString, ReadString);
+
+#undef DefineReadOperator
--- DataScript/DataScriptReader.hpp
+++ DataScript/DataScriptReader.hpp
@@ -2,71 +2,38 @@
 
 #include <string>
 #include <vector>
-#include <sstream>
 #include <unordered_map>
 
-enum class DsStatus
+enum : unsigned char
 {
-	Good,
-	Bad,
-	Eof,
-	BadType,
+	DsBad		= 1 << 0,
+	DsBadtype	= 1 << 1,
+	DsEof		= 1 << 2
 };
 
 class DsReader
 {
 public:
-	// Load source file
-	DsReader(std::string const& file);
 
-	// Returns data, false if type not match
-	template<class Type>
-	void Read(Type& val);
+	DsReader(std::string const& filepath);
 
-	// Identical to Read
-	template<class Type>
-	DsReader& operator>>(Type& val)
-	{
-		Read(val);
-		return *this;
-	}
+	operator unsigned char() const;
+	std::string Message() const;
 
-	// Get current status
-	DsStatus Status() const;
+	DsReader& operator>>(auto& var);
 
-	// Get current read position
-	size_t Position() const;
-
-	// Get section position
-	size_t SectionPosition(std::string const& name) const;
-
-	// goto pointer location
-	void Goto(size_t position);
-
-	// goto section
-	void Goto(std::string const& section);
+	void Goto(unsigned location);
+	unsigned Pointer() const;
+	void Goto(std::string const& label);
+	unsigned Label(std::string const& name) const;
 
 private:
-	// Check if such value is valid
-	bool ValueValidate(size_t begin_ptr) const;
 
-	// Read string as number
-	template<class Type>
-	bool ReadNumber(std::string const& str, Type* output) const;
+	unsigned char state;
+	std::string message;
 
-	// Read as string
-	bool ReadString(std::string const& str, std::string* output) const;
-
-	// Handles commands
-	bool HandleCommand(std::vector<std::string> const& args);
-
-private:
-	enum class PrimitiveType : char;
-	struct RawData { PrimitiveType type; std::string value; };
-	struct CustomType { std::vector<PrimitiveType> primitives; };
-	std::vector<RawData> rdatas;
-	std::unordered_map<std::string, size_t> sections;
-	std::unordered_map<std::string, CustomType> typelist;
-	DsStatus status;
-	size_t read_ptr;
+	struct RawData { char type; std::string value; };
+	std::vector<RawData> raw_datas;
+	std::unordered_map<std::string, unsigned> labels;
+	unsigned read_pointer;
 };
